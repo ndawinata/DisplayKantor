c14cf10e4ece5fb48ca3a8e2e41d246817fd648d
ndawinata <nanda.winata@gmail.com>
2022-01-28 15:07:24 +0800

    coba

--- static/assets/js/myscript.js
+++ static/assets/js/myscript.js
@@ -86,6 +86,28 @@ $(document).ready(function () {
     $('#submit').click(()=>{
         take_snapshot()
     });
+
+    var tabledata = [
+        {id:1, pnbp:"Informasi Cuaca untuk Pelayaran", satuan:"per route \nper hari", tarif:"Rp. 250.000,00"},
+        {id:2, pnbp:"Informasi Cuaca untuk Pelabuhan", satuan:"per route \nper hari", tarif:"Rp. 225.000,00"},
+        {id:3, pnbp:"Informasi Cuaca untuk Pengeboran Lepas", satuan:"per route \nper hari", tarif:"Rp. 330.000,00"},
+    ];
+
+    var table = new Tabulator("#example-table", {
+        data:tabledata, //assign data to table,
+        height:500,
+        layout:"fitColumns", //fit columns to width of table (optional)
+        columns:[ //Define Table Columns
+            {title:"Jenis Penerimaan Negara Bukan Pajak", field:"pnbp", width:300},
+            {title:"Satuan", field:"satuan", width:150},
+            {title:"Tarif", field:"tarif", width:150},
+        ],
+    });
+    
+    //trigger an alert message when the row is clicked
+    table.on("rowClick", function(e, row){ 
+        alert("Row " + row.getData().id + " Clicked!!!!");
+    });
 });
 
 const setJenis = (val) => {
